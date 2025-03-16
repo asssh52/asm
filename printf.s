613502e7e3bd0c949e915dd05cc7999217aabd8a
asssh52 <aptemka9000@gmail.com>
2025-03-16 21:55:08 +0300

    +%b

--- printf.s
+++ printf.s
@@ -21,7 +21,7 @@ _start:
 ;================================================================================
 ;Comm:
 ;In: format line RBX
-;Destr: RCX, RDX
+;Destr: RAX, RCX, RDX
 ;================================================================================
 fillBuff:
 
@@ -36,16 +36,16 @@ fillBuff:
     cmp rdx, BuffSize            ; if buffsize >= maxsize
     jge emptyBuff
 
-    mov ah, [rbx + rcx]
+    mov al, [rbx + rcx]
 
-    cmp ah, '%'                 ; if char == %
+    cmp al, '%'                 ; if char == %
     je processArg
 
-    cmp ah, 0h                  ; if char == 0
+    cmp al, 0h                  ; if char == 0
     je endLoop
 
 
-    mov [Buffer + rdx], ah      ; load char to buff
+    mov [Buffer + rdx], al      ; load char to buff
     inc rcx
     inc rdx
     jmp processCharLoop
@@ -54,11 +54,11 @@ fillBuff:
     jmp processArgEnd
     processArg:
     inc rcx
-    mov ah, [rbx + rcx]
+    mov al, [rbx + rcx]
 
-    cmp ah, '%'
+    cmp al, '%'
     jg percentEnd
-    mov [Buffer + rdx], ah
+    mov [Buffer + rdx], al
     inc rcx
     inc rdx
     jmp processCharLoop
@@ -69,7 +69,6 @@ fillBuff:
     shl rax, 3d
     add rax, jumpTable
     inc rcx
-    inc rdx
 
     jmp [rax]
 
@@ -104,10 +103,11 @@ fillBuff:
 ;----------------------
 
     printBin:
-
-
-
-
+        mov r11, Buffer
+        mov rsi, 0ffffh
+        mov rdi, 2h
+        call printNum
+        jmp processCharLoop
     endLoop:
 ;-=-=-=-=-=-=-=-=-=-=
     ret
@@ -115,6 +115,109 @@ fillBuff:
 ;================================================================================
 
 
+;================================================================================
+;Comm: prints num to buffer, in different notations.
+;In: R11 + RDX - place in buffer,  RSI - number, RDI - notation (2, 8, 10, 16)
+;Destr: RAX, R8
+;================================================================================
+printNum:
+
+    cmp rdi, 2h
+    je pBin
+
+    cmp rdi, 8h
+    je pOct
+
+    ;cmp rdi, 0ah
+    ;je pDec
+
+    jmp pHex
+
+    pBin:
+        mov r8, 1h
+        jmp mainPrnt
+
+    pOct:
+        mov r8, 3h
+        jmp mainPrnt
+
+    pHex:
+        mov r8, 4h
+        jmp mainPrnt
+
+    mainPrnt:
+        sub rdi, 1h
+
+    loopPrnt:
+        mov rax, rsi
+        and rax, rdi
+
+        mov r10, rax
+        add r10, alphabet
+
+        xor rax, rax
+        mov al, [r10]
+        mov r10b, al
+
+        call putChar
+
+        push rcx
+        mov rcx, r8
+        shr rsi, cl
+        pop rcx
+
+        cmp rsi, 0h
+        jne loopPrnt
+
+    pNumEnd:
+    ret
+
+;================================================================================
+
+
+
+
+;================================================================================
+;Comm: prints one char to buff, clears and prints the buffer if it is full.
+;Comm: saves RCX, might destr other regs
+;In: R11 - buff, RDX - current pos in buff, R10 - char to print.
+;Destr: ?
+;================================================================================
+putChar:
+    cmp rdx, BuffSize
+    je clearBuff
+
+    mov [r11 + rdx], r10b
+    inc rdx
+
+    jmp endClearBuff
+
+    clearBuff:
+    push rax
+    push rbx
+    push rcx
+    push rdi
+    push rsi
+    push r10
+    push r11
+
+    call printBuff
+
+    pop r11
+    pop r10
+    pop rsi
+    pop rdi
+    pop rcx
+    pop rbx
+    pop rax
+
+    xor rdx, rdx
+
+    endClearBuff:
+
+    ret
+
+;================================================================================
 
 
 
@@ -153,5 +256,6 @@ exitProg:
 
 section     .data
 
-Buffer:     db "0000000000000000"
-formatLine: db "meow %% meow %%", 0ah, 0h
+Buffer:     times BuffSize db "0"
+formatLine: db "%b meow %b", 0ah, 0h
+alphabet:   db "0123456789abcdef"
